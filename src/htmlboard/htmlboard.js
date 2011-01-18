b406856a4fe7ad64e6d3dbb82e5743b5bc4cd2bc
Darren Cauthon <darren@cauthon.com>
2011-01-18 06:48:09 -0600

    Add rows.

--- src/htmlboard/htmlboard.js
+++ src/htmlboard/htmlboard.js
@@ -5,13 +5,13 @@
         var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
         for (column in columns) {
-            for (var index = 0; index < 8; index++) {
+            for (var index = 1; index <= 8; index++) {
                 var square = $('<div />');
                 square.addClass(columns[column]);
+                square.addClass(index + '');
                 $(this).append(square);
             }
         }
-
     };
 
 })(jQuery);
