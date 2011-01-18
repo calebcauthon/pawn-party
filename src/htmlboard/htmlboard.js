112fcf6e66a3cba02bc42c4bb3e1a34f4b034b74
Darren Cauthon <darren@cauthon.com>
2011-01-17 22:59:03 -0600

    Add more tests.

--- src/htmlboard/htmlboard.js
+++ src/htmlboard/htmlboard.js
@@ -1,8 +1,16 @@
 (function ($) {
 
     jQuery.fn.setupBoard = function () {
-        for (var index = 0; index < 64; index++)
-            $(this).append('<div />');
+        for (var index = 0; index < 64; index++) {
+            if (index < 8)
+                $(this).append('<div class="A" />');
+            else if (index >= 8 && index < 16)
+                $(this).append('<div class="B" />');
+            else if (index >= 16 && index < 24)
+                $(this).append('<div class="C" />');
+            else
+                $(this).append('<div />');
+        }
     };
 
 })(jQuery);
