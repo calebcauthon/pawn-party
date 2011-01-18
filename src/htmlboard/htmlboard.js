8fcc104da07345152bd35c029ffbca2be5c57d60
Darren Cauthon <darren@cauthon.com>
2011-01-18 06:36:22 -0600

    Add the rest of the columns.

--- src/htmlboard/htmlboard.js
+++ src/htmlboard/htmlboard.js
@@ -1,16 +1,15 @@
 (function ($) {
 
     jQuery.fn.setupBoard = function () {
-        for (var index = 0; index < 64; index++) {
-            if (index < 8)
-                $(this).append('<div class="A" />');
-            else if (index >= 8 && index < 16)
-                $(this).append('<div class="B" />');
-            else if (index >= 16 && index < 24)
-                $(this).append('<div class="C" />');
-            else
-                $(this).append('<div />');
+
+        var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+        for (column in columns) {
+            for (var index = 0; index < 8; index++) {
+                $(this).append('<div class="' + columns[column] + '" />');
+            }
         }
+
     };
 
 })(jQuery);
